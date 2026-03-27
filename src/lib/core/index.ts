229facd8f33f0a1e0a1b842643027ec7e7fff1ed
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2026-03-27 08:35:32 +0800

    chore(cleanup): delete old monolithic repository structure code

--- src/lib/core/index.ts
+++
@@ -1,7 +0,0 @@
-export { InkStory } from './InkStory';
-export { Tags, splitAtCharacter } from './Tags';
-export { Parser } from './Parser';
-export { Patches } from './Patches';
-export { ExternalFunctions } from './ExternalFunctions';
-export { ChoiceParser, Choice } from './ChoiceParser';
-export type { ChoiceProps } from './ChoiceParser';
--- src/lib/stores/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as useStory } from './storyStore';
-export { default as useContents, CHOICE_SEPARATOR } from './contentsStore';
-export { default as useChoices } from './choicesStore';
-export { default as useVariables } from './variablesStore';
-export { default as createSelectors } from './createSelectors';
