4596b1cc44790fc553b3b45ea8b034e1f04599fe
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2026-04-02 22:40:59 +0800

    refactor(test): move testing utilities to root directory
    - Move testing-library.ts from packages/react/test to root test directory
    - Remove matchers.d.ts from packages/react/test
    - Make testing utilities available to all packages

--- packages/react/test/matchers.d.ts
+++
@@ -1,7 +0,0 @@
-import type { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
-import type { Matchers, AsymmetricMatchers } from "bun:test";
-
-declare module "bun:test" {
-  interface Matchers<T> extends TestingLibraryMatchers<typeof expect.stringContaining, T> {}
-  interface AsymmetricMatchers extends TestingLibraryMatchers {}
-}
