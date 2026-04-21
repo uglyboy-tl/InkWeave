1ec49f8d067e521fe4040dbddff2f61eba9177b3
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2026-04-21 14:46:09 +0800

    feat(audio-plugin): add music state persistence with Zustand

--- packages/plugins/src/audio/index.ts
+++ packages/plugins/src/audio/index.ts
@@ -1,5 +1,16 @@
 import { Events, type FileHandler, type InkStory, Patches, Tags } from "@inkweave/core";
+import { create } from "zustand";
 import { AudioController } from "./AudioController";
+
+type StoryMusic = {
+  music: string;
+  setMusic: (music: string) => void;
+};
+
+export const useStoryMusic = create<StoryMusic>((set) => ({
+  music: "",
+  setMusic: (music) => set({ music }),
+}));
 
 const getPath = (path: string, fileHandler?: FileHandler) => {
   if (fileHandler && "resolveFilename" in fileHandler) {
@@ -22,13 +33,28 @@ const load = () => {
     if (val) {
       AudioController.cleanupMusic();
       AudioController.set_music(getPath(val, ink.options.fileHandler));
+      useStoryMusic.getState().setMusic(getPath(val, ink.options.fileHandler));
     } else {
       AudioController.cleanupMusic();
     }
   });
 
   Patches.add(function () {
+    Object.defineProperty(this, "music", {
+      get() {
+        return useStoryMusic.getState().music;
+      },
+      set(path: string) {
+        useStoryMusic.getState().setMusic(path);
+        // 如果设置了音乐路径且当前没有音乐在播放，则启动播放
+        if (path && !AudioController.music) {
+          AudioController.set_music(path);
+        }
+      },
+    });
+    this.save_label.push("music");
     this.audio = AudioController;
+
     const cleanupAudio = () => {
       AudioController.cleanupSound();
       AudioController.cleanupMusic();
